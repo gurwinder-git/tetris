45506a6f7dae6122dc15d16604aeb72aada09cae
Gurwinder Singh <guri60343@gmail.com>
2021-08-12 12:03:02 +0530

    added shapes array default state

--- src/App.js
+++ src/App.js
@@ -3,16 +3,24 @@ import './App.css'
 import Game from './components/Game/Game'
 import Controls from './components/controls/Controls';
 import LeftDiv from './components/LeftDiv/LeftDiv'
-import MassagePopUp from './components/UI/MessagePopUp/MassagePopUp';
+// import MassagePopUp from './components/UI/MessagePopUp/MassagePopUp';
+
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducers from './store/reducers/combineReducer';
+
+const store = createStore(reducers)
 
 function App() {
   return (
-    <GameContainer>
-      {/* <MassagePopUp massage="Paused" /> */}
-      <LeftDiv />
-      <Game />
-      <Controls />
-    </GameContainer>
+    <Provider store={store}>
+      <GameContainer>
+        {/* <MassagePopUp massage="Paused" /> */}
+        <LeftDiv />
+        <Game />
+        <Controls />
+      </GameContainer>
+    </Provider>
   );
 }
 
